209d32efadfd24a9edf4a52ac68a112893f571de
Oleg <olegsiljanovs5@gmail.com>
2019-10-01 20:22:34 +0300

    Improved

--- src/Oleg/Main.java
+++ src/Oleg/Main.java
@@ -2,6 +2,7 @@ package Oleg;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,19 +11,16 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        System.out.println("Enter your guess: ");
         String answer;
 
         do {
 
 
             boolean userWon = false;
+            int myNum = rand.nextInt(100) + 1;
 
             for (int i = 0; i < 10; i++) {
-
-                int myNum = rand.nextInt(100) + 1;
-                int userNum = scan.nextInt();
+                int userNum = askInt("Enter your guess: ", 1, 100);
                 {
 
                     if (myNum == userNum) {
@@ -45,11 +43,45 @@ public class Main {
 
 
             }
-            System.out.println("Would you like to play again? (y/n)");
 
-            answer = scan.next();
-        } while (answer.equals("y"));
+
+        } while (askStr("Do you want to play again?"));
+
+
+        }
+
+    static int askInt(String msg, int min, int max) {
+        while (true) {
+            try {
+                System.out.println(msg);
+                int answer = scan.nextInt();
+                if (answer >= min && answer <= max) {
+                    return answer;
+                }
+            } catch (InputMismatchException ex) {
+                System.out.println("It isn't a number");
+                scan.next();
+}
+            System.out.printf("Please enter number from %d to %d\n", min, max);
+        }
 
     }
-}
+
+    static boolean askStr(String msg) {
+        while (true) {
+            System.out.println(msg);
+            String answer = scan.next();
+            boolean isY = answer.equalsIgnoreCase("y");
+            boolean isN = answer.equalsIgnoreCase("n");
+            if (isY || isN) {
+                return isY;
+            }
+            System.out.println("Enter 'y' or 'n'");
+        }
+    }
+
+
+
+    }
+
 
